759067f45df6d540a95921f358548191e2f96251
Rahul Bajaj <therbajaj@gmail.com>
2021-06-09 12:42:38 -0400

    route fix #2

--- server/index.js
+++ server/index.js
@@ -47,11 +47,11 @@ app.get('/:id', (req, res) => {
 
 //heroku custom variable
 if (process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use(express.static('../client/build'));
 }
 
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
