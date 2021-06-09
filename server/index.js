e4177591fc41272ab2e360ab4b1b44d8c9a1e4ee
Rahul Bajaj <therbajaj@gmail.com>
2021-06-09 12:25:27 -0400

    fixing route bug

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -46,8 +47,12 @@ app.get('/:id', (req, res) => {
 
 //heroku custom variable
 if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 }
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 
 app.listen(port, () => {
